1d40f32a51c0603940ee5dbc4a55b331ba21b00b
Kenneth Ofosu <kofosu89@gmail.com>
2019-10-01 13:16:31 -0400

    bind Apollo Server to respond to requests on GraphQL enpoints

--- src/server/index.js
+++ src/server/index.js
@@ -3,25 +3,41 @@ import path from 'path';
 import helmet from 'helmet';
 import cors from 'cors';
 import compress from 'compression';
+import services from './services';
 
 const app = express();
 const root = path.join(__dirname, '../../');
 
-app.use(cors());
-app.use(compress());
-app.use(helmet());
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", "'unsafe-inline'"],
-    styleSrc: ["'self'", "'unsafe-inline'"],
-    imgSrc: ["'self'", "data:", "*.amazonaws.com"],
-  },
-}));
+if (process.env.NODE_ENV === 'production') {
+  app.use(helmet());
+  app.use(helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      imgSrc: ["'self'", "data:", "*.amazonaws.com"],
+    },
+  }));
+  app.use(compress());
+  app.use(cors());
+}
 app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
 
 app.use('/', express.static(path.join(root, 'dist/client')));
+
 app.use('/uploads', express.static(path.join(root, 'uploads')));
+
+const serviceNames = Object.keys(services);
+
+for (let i = 0; i < serviceNames.length; i += 1) {
+  const name = serviceNames[i];
+  if (name === 'graphql') {
+    services[name].applyMiddleware({ app });
+  } else {
+    app.use(`/${name}`, services[name]);
+  }
+}
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(root, '/dist/client/index.html'));
 });
